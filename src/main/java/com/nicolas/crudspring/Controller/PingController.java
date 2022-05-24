9e0fccde2561071547333d111848f215e443d698
Nicolas Portela <nmpc1997@gmail.com>
2022-05-24 17:40:31 -0300

    primeiro commit

---
+++ src/main/java/com/nicolas/crudspring/Controller/PingController.java
@@ -0,0 +1,13 @@
+package com.nicolas.crudspring.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PingController {
+
+    @GetMapping("/ping")
+    public String ping(){
+        return "Funfando";
+    }
+}
